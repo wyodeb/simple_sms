8120185fa86303d319e551da29edecdb606fe555
wyodeb <hello@wyodeb.io>
2023-03-13 14:45:09 +0200

    routs adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       match 'authentication', to: 'authentication#login', via: :post
-      match 'inbound', to: 'messages#inbound', via: :post
-      match 'outbound', to: 'messages#outbound', via: :post
+      match 'inbound/sms', to: 'messages#inbound', via: :post
+      match 'outbound/sms', to: 'messages#outbound', via: :post
     end
   end
 end
